0ea90557eec032df1c9d6647e44d64e1ffd8160b
jeanc413 <jeanc.fernandez@gmail.com>
2023-11-14 18:57:09 +0100

    Created template to run a defined experiment. Unfinished.

---
+++ experiment.py
@@ -0,0 +1,144 @@
+from functools import partial
+
+import numpy as np
+
+import generator
+import clustering
+
+
+def linear_drift(x: np.ndarray, mu: np.ndarray, beta: np.ndarray):
+    return mu * x + beta
+
+
+def constant_diffusion(*_, constant: np.ndarray):
+    return constant
+
+
+def gbm_drift_and_diffusion(x: np.ndarray, mu: np.ndarray):
+    return x * mu
+
+
+def oup_diffusion(x: np.ndarray, theta: np.ndarray, mean: np.ndarray):
+    """Ornstein-Uhlenbeck Process"""
+    return theta * (mean - x)
+
+
+seed = 123
+
+len_seed = np.random.default_rng(seed)
+len_gen = partial(len_seed.integers, low=45, high=55)
+
+experiment_seed = np.random.default_rng(seed)
+
+experiment = generator.Experiment(
+    series_sets={
+        "lineal drift and constant diffusion": generator.TimeSeriesSet(
+            train_n_ts=20,
+            clusters_definitions=[
+                generator.TimeSeries(
+                    sz=len_gen,
+                    drift=partial(
+                        linear_drift,
+                        mu=np.array([1.5, 2, 2.5]),
+                        beta=np.array([-15, -20, -25]),
+                    ),
+                    diffusion=partial(
+                        constant_diffusion, constant=np.array([-7.5, -10, -12.5])
+                    ),
+                    initial_value=np.array([0.5, 13.5, -0.3]),
+                    time_step=0.1,
+                )
+            ],
+            seed=experiment_seed,
+            test_n_ts=10,
+        )
+    },
+    partial_models={
+        "km-euclidean": partial(
+            clustering.CKMeans,
+            distanca_measure="euclidean",
+            state=np.random.default_rng(seed),
+            max_iterations=25,
+        ),
+        "km-dtw": partial(
+            clustering.CKMeans,
+            distanca_measure="dtw",
+            state=np.random.default_rng(seed),
+            max_iterations=25,
+        ),
+        "km-soft_dtw-g=0.5": partial(
+            clustering.CKMeans,
+            distanca_measure="soft_dtw",
+            state=np.random.default_rng(seed),
+            max_iterations=25,
+            gamma=0.5,
+        ),
+        "km-soft_dtw-g=1": partial(
+            clustering.CKMeans,
+            distanca_measure="soft_dtw",
+            state=np.random.default_rng(seed),
+            max_iterations=25,
+            gamma=1.0,
+        ),
+        "km-soft_dtw-g=2": partial(
+            clustering.CKMeans,
+            distanca_measure="soft_dtw",
+            state=np.random.default_rng(seed),
+            max_iterations=25,
+            gamma=2,
+        ),
+        "km-soft_dtw-g=3": partial(
+            clustering.CKMeans,
+            distanca_measure="soft_dtw",
+            state=np.random.default_rng(seed),
+            max_iterations=25,
+            gamma=3,
+        ),
+        "sc-euclidean": partial(
+            clustering.DBScan,
+            distance_base="euclidean",
+            state=np.random.default_rng(seed),
+            epsilon=300,
+            min_pts=5,
+        ),
+        "sc-dtw": partial(
+            clustering.DBScan,
+            distance_base="dtw",
+            state=np.random.default_rng(seed),
+            epsilon=300,
+            min_pts=5,
+        ),
+        "sc-soft_dtw-g=0.5": partial(
+            clustering.DBScan,
+            distance_base="soft_dtw",
+            state=np.random.default_rng(seed),
+            epsilon=300,
+            min_pts=5,
+            distance_base_kwargs={"g": 0.5},
+        ),
+        "sc-soft_dtw-g=1": partial(
+            clustering.DBScan,
+            distance_base="soft_dtw",
+            state=np.random.default_rng(seed),
+            epsilon=300,
+            min_pts=5,
+            distance_base_kwargs={"g": 1},
+        ),
+        "sc-soft_dtw-g=2": partial(
+            clustering.DBScan,
+            distance_base="soft_dtw",
+            state=np.random.default_rng(seed),
+            epsilon=300,
+            min_pts=5,
+            distance_base_kwargs={"g": 2},
+        ),
+        "sc-soft_dtw-g=3": partial(
+            clustering.DBScan,
+            distance_base="soft_dtw",
+            state=np.random.default_rng(seed),
+            epsilon=300,
+            min_pts=5,
+            distance_base_kwargs={"g": 3},
+        ),
+    },
+)
