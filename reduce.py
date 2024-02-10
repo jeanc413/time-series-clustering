3f75c5eeacaf5308eb9d0c6ca7571f98e7a051d2
JeanC <jeanc.fernandez@gmail.com>
2024-02-10 12:58:32 +0100

    Added functions to parse model parameters into easier to read references to the used model, distance measures and gamma values. Refactored summarization to use more user friendly model references.

--- reduce.py
+++ reduce.py
@@ -4,15 +4,33 @@ from pathlib import Path
 import pandas as pd
 
 from experiment import linear_drift, constant_diffusion, gbm_drift_and_diffusion, oup_drift, trigonometric_drift
-from clustering import CKMeans, DBScan
+from clustering import CKMeans
 
 
 def refactor(experiment_result: dict):
     refactored_results = {"simulation_mode": ' '.join(experiment_result["series_name"].split(' ')[:-1]),
-                          "algorithm": CKMeans if issubclass(experiment_result['alg_model'], CKMeans) else DBScan,
+                          "algorithm": "K-Means" if issubclass(experiment_result['alg_model'].func,
+                                                               CKMeans) else "DBSCAN",
                           **experiment_result['alg_model'].keywords}
 
     return refactored_results
+
+
+def try_get_gamma(gamma: dict[str, float]):
+    try:
+        gamma = gamma['gamma']
+    except TypeError:
+        pass
+    return gamma
+
+
+def refactor_distance_measure(frame_section: pd.DataFrame):
+    distance_measure = frame_section['distance_measure'].tolist()
+    distance_measure = [b if pd.isnull(m) else m for m, b in
+                        zip(distance_measure, frame_section['distance_base'].tolist())]
+    gamma = frame_section['gamma'].tolist()
+    gamma = [try_get_gamma(b) if pd.isnull(m) else m for m, b in zip(gamma, frame_section['distance_base_kwargs'])]
+    return [d if pd.isnull(g) else f'{d}, gamma={g}' for d, g in zip(distance_measure, gamma)]
 
 
 results = []
@@ -25,7 +43,9 @@ for p in Path("./experiments-results").glob("*.experiment"):
 data = pd.DataFrame(results)
 data.to_excel("raw_data.xlsx")
 summarized_results = (data
-                      .assign(alg=lambda df: df.alg_model.apply(str))
-                      .groupby(['simulation_mode', 'alg'])
+                      .assign(algorithm=lambda df: df.algorithm.apply(str),
+                              distance_measure=lambda df: refactor_distance_measure(df))
+                      .drop(columns=['iterations'])
+                      .groupby(['simulation_mode', 'algorithm', 'distance_measure'])
                       .describe())
 summarized_results.to_excel("summary.xlsx")
